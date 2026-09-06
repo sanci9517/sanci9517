31608c38ca6581b6682cafa7d94479d4442493d5
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-06 21:57:10 +0200

    Add centralized navigation data

---
+++ data/navigation.js
@@ -0,0 +1,12 @@
+export const navigation = Object.freeze([
+  {
+    label: 'Főoldal',
+    path: '/',
+    type: 'route',
+  },
+  {
+    label: 'Twitch',
+    urlKey: 'twitch',
+    type: 'external',
+  },
+]);
