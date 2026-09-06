ff5764c5bb858aa8c12aca6ecbb7ee91f50fb6f8
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-06 21:23:36 +0200

    foundation: add central site data

---
+++ data/site.js
@@ -0,0 +1,8 @@
+export const site = {
+  brand: 'Sanci9517',
+  language: 'hu',
+  description: 'Sanci9517 hivatalos streamer oldala.',
+  links: {
+    twitch: 'https://www.twitch.tv/sanci9517'
+  }
+};
