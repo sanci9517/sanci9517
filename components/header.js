9dabbb7e3d5ccde52a0622b273d67374275a5073
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-06 21:57:37 +0200

    Keep header routing configuration centralized

--- components/header.js
+++ components/header.js
@@ -1,9 +1,10 @@
+import { siteUrl } from '../core/config.js';
 import { Navigation } from './navigation.js';
 
 export function Header(site) {
   return `
     <header class="site-header">
-      <a class="brand" href="/sanci9517/" data-route aria-label="${site.brand} főoldal">
+      <a class="brand" href="${siteUrl('/')}" data-route aria-label="${site.brand} főoldal">
         <span class="brand-mark" aria-hidden="true">S</span>
         <span class="brand-name">Sanci</span>
       </a>
