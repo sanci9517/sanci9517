9fa7c2e8079d7ec14ed392b07d07bb9f5f6a8ade
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-07 21:25:15 +0200

    Add modular Twitch frontend data loaders

--- twitch/status.js
+++ twitch/status.js
@@ -3,3 +3,15 @@ import { api } from '../core/api.js';
 export async function loadTwitchStatus() {
   return api.get('/twitch/status');
 }
+
+export async function loadTwitchChannel() {
+  return api.get('/twitch/channel');
+}
+
+export async function loadTwitchFollowers() {
+  return api.get('/twitch/followers');
+}
+
+export async function loadTwitchData() {
+  return api.get('/twitch/data');
+}
