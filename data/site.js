9b6291ddc0ca5f0fd9f4ca5b09315f219f3b86f2
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-07 04:28:39 +0200

    Connect saved website settings to runtime site data

--- data/site.js
+++ data/site.js
@@ -1,8 +1,22 @@
-export const site = {
+import { storage } from '../core/storage.js';
+
+const SITE_SETTINGS_KEY = 'admin-website-settings';
+
+export const site = Object.freeze({
   brand: 'Sanci9517',
   language: 'hu',
   description: 'Sanci9517 hivatalos streamer oldala.',
   links: {
     twitch: 'https://www.twitch.tv/sanci9517'
   }
-};
+});
+
+export function getSite() {
+  const saved = storage.get(SITE_SETTINGS_KEY, {});
+  return {
+    ...site,
+    brand: saved.brand || site.brand,
+    description: saved.description || site.description,
+    links: { ...site.links },
+  };
+}
