8319185c9d8e1ee1fd3a9268e8a19599004b1f76
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-08 08:06:42 +0200

    Remove obsolete admin navigation bridge

--- admin/entry.js
+++ admin/entry.js
@@ -1,11 +1,7 @@
 import { renderAdmin } from './index.js';
-import { renderAdminDashboard } from './dashboard.js';
 
 const root = document.querySelector('#app');
 
 if (root) {
-  window.__SANCI_ADMIN_NAVIGATE__ = (path) => {
-    if (path === '/admin') renderAdminDashboard(root);
-  };
   renderAdmin(root);
 }
