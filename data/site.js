a34b3a5f280abbed792c59edb78119b645a922e1
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-08 05:31:45 +0200

    Remove duplicate site-state reader from data defaults

--- data/site.js
+++ data/site.js
@@ -1,7 +1,3 @@
-import { storage } from '../core/storage.js';
-
-const SITE_SETTINGS_KEY = 'site-settings';
-
 export const site = Object.freeze({
   brand: 'Sanci9517',
   language: 'hu',
@@ -10,13 +6,3 @@ export const site = Object.freeze({
     twitch: 'https://www.twitch.tv/sanci9517',
   },
 });
-
-export function getSite() {
-  const saved = storage.get(SITE_SETTINGS_KEY, {});
-  return {
-    ...site,
-    brand: saved.brand || site.brand,
-    description: saved.description || site.description,
-    links: { ...site.links, ...(saved.links || {}) },
-  };
-}
