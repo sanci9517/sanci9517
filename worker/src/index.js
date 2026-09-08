4b841e3c7ea7e3fc5a3b39c97d02c28d2e9bc34d
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-08 07:45:36 +0200

    Trigger Worker redeploy after config repair

--- worker/src/index.js
+++ worker/src/index.js
@@ -7,7 +7,7 @@ const DEFAULT_ADMIN_ORIGIN = 'https://sanci9517.github.io';
 const YOUTUBE_API = 'https://www.googleapis.com/youtube/v3';
 const BROADCASTER_LOGIN = 'sanci9517';
 const PUBLIC_SETTING_KEYS = ['site-settings', 'navigation-settings-v2', 'page-settings'];
-const WORKER_VERSION = 'admin-auth-bearer-1';
+const WORKER_VERSION = 'admin-auth-bearer-2';
 
 function corsHeaders(request, env) {
   const origin = request.headers.get('origin');
