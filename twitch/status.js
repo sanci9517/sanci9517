8f70cf48c5f8caaf825f9219aff59efa9a86af03
sanci9517 <sandor.bogadi95@gmail.com>
2026-09-08 05:32:21 +0200

    Remove unsupported Twitch follower client loader

--- twitch/status.js
+++ twitch/status.js
@@ -1,21 +1,6 @@
 import { api } from '../core/api.js';
 
-export async function loadTwitchStatus() {
-  return api.get('/twitch/status');
-}
-
-export async function loadTwitchChannel() {
-  return api.get('/twitch/channel');
-}
-
-export async function loadTwitchFollowers() {
-  return api.get('/twitch/followers');
-}
-
-export async function loadTwitchStatistics() {
-  return api.get('/twitch/statistics');
-}
-
-export async function loadTwitchData() {
-  return api.get('/twitch/data');
-}
+export async function loadTwitchStatus() { return api.get('/twitch/status'); }
+export async function loadTwitchChannel() { return api.get('/twitch/channel'); }
+export async function loadTwitchStatistics() { return api.get('/twitch/statistics'); }
+export async function loadTwitchData() { return api.get('/twitch/data'); }
